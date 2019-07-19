fffdefc4666f2116058ccb1443fa119ae57a1139
Ted Mader <tlmader.dev@gmail.com>
2019-07-19 13:30:29 -0500

    Add gatsby-intro and create initial pages

---
+++ src/pages/index.js
@@ -0,0 +1,9 @@
+import React from 'react';
+import { Link } from 'gatsby';
+export default () => (
+  <>
+    <h1>Hi, Frontend Masters!</h1>
+    <p>Hello Minnesota!</p>
+    <Link to="/about/">Learn about me &rarr;</Link>
+  </>
+);
